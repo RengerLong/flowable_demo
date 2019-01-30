492a59461a7a69a91317141d738422dcd9f0eb17
rengar <17513160558@163.com>
2019-01-30 15:49:10 +0800

    请假单数据库表

---
+++ 123.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.7.9-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `a_leave` */
+
+DROP TABLE IF EXISTS `a_leave`;
+
+CREATE TABLE `a_leave` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `startDate` date NOT NULL,
+  `numDays` int(11) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `isok` varchar(20) NOT NULL DEFAULT '待审批',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `a_leave` */
+
+insert  into `a_leave`(`id`,`name`,`startDate`,`numDays`,`description`,`isok`) values (1,'nie233','2019-01-02',1,'该吃饭了','待审批');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
